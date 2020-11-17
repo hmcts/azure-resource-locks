87141d4954086cada4bcf14fe62a3bf05c64310d
Avinash Mutham <avinashm@Torreira.local>
2020-11-17 14:19:23 +0000

    checking for specific resource types

--- scripts/enable-resource-locking.sh
+++ scripts/enable-resource-locking.sh
@@ -4,12 +4,13 @@ set -e
 SUBS=$1
 
 echo "retrieve all resource groups in a subscription"
-RESOURCE_GROUPS=$(az group list | jq '.[].name')
+RESOURCE_GROUPS=$(az group list --subscription DCD-CFTAPPS-SBOX | jq '.[].name')
 
 for rg in ${RESOURCE_GROUPS[@]}; 
 do
   RG_NAME=$(sed -e 's/^"//' -e 's/"$//' <<<"$rg")
-
+  RSRC_LIST=$(az resource list -g $RG_NAME --subscription DCD-CFTAPPS-SBOX | jq '.[].type' | sed -e 's/^"//' -e 's/"$//' | grep -ciE 'Storage|KeyVault|Sql')
+  echo "Total no of resources matched: $RSRC_LIST"
   echo "retrieving locks for each resource group in the subscription: "
   locks=$(az lock list -g $RG_NAME -o tsv)
 
